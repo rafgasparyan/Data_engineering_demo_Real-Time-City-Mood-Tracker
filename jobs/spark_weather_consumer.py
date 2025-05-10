43353dd88b237b50fdde44c1ffb79bc96972bc53
rafgasparyan <rgasparyan@griddynamics.com>
2025-05-10 16:04:21 +0400

    remove unnecessary imports and comments

--- jobs/spark_weather_consumer.py
+++ jobs/spark_weather_consumer.py
@@ -1,8 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, DoubleType, StringType, TimestampType
-from pymongo import MongoClient
-from stream_utils.kafka_reader import read_kafka_stream
+from stream_utils.utils import read_kafka_stream
 from jobs.stream_utils.schemas import weather_schema
 from jobs.stream_utils.utils import write_to_mongo_factory
 
@@ -24,16 +21,6 @@ df_parsed = read_kafka_stream(
 
 
 write_to_mongo = write_to_mongo_factory("weather_events", log_prefix="WEATHER")
-
-
-# def write_to_mongo(batch_df, batch_id):
-#     records = batch_df.toPandas().to_dict("records")
-#     if records:
-#         client = MongoClient("mongodb://mongo:27017/")
-#         db = client["city_mood"]
-#         collection = db["weather_events"]
-#         collection.insert_many(records)
-#         client.close()
 
 df_parsed.writeStream \
     .foreachBatch(write_to_mongo) \
